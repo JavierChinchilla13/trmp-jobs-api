9b5777e374910b3d4e5a57c81b83a4b8219240b6
Javier <javidilugo@gmail.com>
2024-10-02 20:23:33 -0600

    commit

--- app.js
+++ app.js
@@ -22,11 +22,22 @@ const jobsRouter = require('./routes/jobs')
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(xss());
 app.use(rateLimiter());
+
+app.get('/', (req, res) => {
+  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
+});
 
 // routes
 app.use('/api/v1/auth',authRouter)
@@ -35,7 +46,7 @@ app.use('/api/v1/jobs',authenticateUser, jobsRouter)
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
